b6504e7c53725db936eaefdde7bf8fed5b11c572
Cheaucapik <oceane.huynh@etu.u-paris.fr>
2025-05-04 18:18:38 +0200

    fin de partie comptabilisée Cartes.vb renommé en jeu.vb

--- S201/Enregistrement.vb
+++ S201/Enregistrement.vb
@@ -5,14 +5,6 @@
         Public Record As Double
         Public NbPartie As Integer
         Public Temps As Double
-
-        Public Sub New(Nom As String, Score As Integer, Record As Double, NbPartie As Integer, Temps As Double)
-            Me.Nom = Nom
-            Me.Score = Score
-            Me.Record = Record
-            Me.NbPartie = NbPartie
-            Me.Temps = Temps
-        End Sub
     End Structure
 
     Dim TJOUE As Joueur()
